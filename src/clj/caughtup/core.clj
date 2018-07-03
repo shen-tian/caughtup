d2192e4669a82741ef5d4b448bfe653d601e605e
Shen Tian <biozinc@gmail.com>
2018-07-03 19:34:36 +0200

    Initial commit

---
+++ src/clj/caughtup/core.clj
@@ -0,0 +1,1 @@
+(ns caughtup.core)
---
+++ src/cljs/caughtup/core.cljs
@@ -0,0 +1,40 @@
+(ns caughtup.core
+  (:require
+   [reagent.core :as reagent]
+   ))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vars
+
+(defonce app-state
+  (reagent/atom {}))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Page
+
+(defn page [ratom]
+  [:div
+
+   [:h5 "You've seen all new posts from the past 21 hours"]])
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Initialize App
+
+(defn dev-setup []
+  (when ^boolean js/goog.DEBUG
+    (enable-console-print!)
+    (println "dev mode")
+    ))
+
+(defn reload []
+  (reagent/render [page app-state]
+                  (.getElementById js/document "app")))
+
+(defn ^:export main []
+  (dev-setup)
+  (reload))
